f40dda6e0036fe09775cf40176b7e006a0934eab
Drin <DrinMontana@gmail.com>
2010-04-25 20:36:09 -0700

    modified getOperator

--- mile2/Instruction.java
+++ mile2/Instruction.java
@@ -7,9 +7,13 @@ public class Instruction {
    private int register1;
    private int register2;
    private int result;
+   private int immediate;
    
    public Instruction(Operator op, int register1, int immediate) {
-      this(op, register1, immediate);
+      this.op = op;
+      this.register1 = register1;
+      this.register2 = register2;
+      this.immediate = immediate;
    }
    
    public Instruction(Operator op, int register1, int register2, int result) {
@@ -19,52 +23,73 @@ public class Instruction {
       this.result = result;
    }
    
-/*
    public static Operator getOperator(String op) {
       //EQ | LT | GT | NE | LE | GE
-      Operator returns;
-      if(op.equals("EQ"))
-         returns = Operator.EQ;
+      //Operator returns;
+      if (op.equals("AND"))
+         return Operator.AND;
+      else if (op.equals("OR"))
+         return Operator.OR;
+      else if(op.equals("EQ"))
+         return Operator.CBREQ;
+      else if (op.equals("LT"))
+         return Operator.CBRLT;
+      else if (op.equals("GT"))
+         return Operator.CBRGT;
+      else if (op.equals("NE"))
+         return Operator.CBRNE;
+      else if (op.equals("LE"))
+         return Operator.CBRLE;
+      else if (op.equals("GE"))
+         return Operator.CBRGE;
+      else if (op.equals("PLUS"))
+         return Operator.ADD;
+      else if (op.equals("MINUS"))
+         return Operator.SUB;
+      else if (op.equals("TIMES"))
+         return Operator.MULT;
+      else if (op.equals("DIVIDE"))
+         return Operator.DIV;
+      return null;
    }
-*/
    public enum Operator {
       ADD,
       ADDI,
-      DIV,
-      MULT,
-      SUB,
-      RSUBI,
       AND,
-      OR,
-      XORI,
-      COMP,
-      COMPI,
+      CALL,
       CBREQ,
       CBRGE,
       CBRGT,
       CBRLE,
       CBRLT,
       CBRNE,
+      COMP,
+      COMPI,
+      COMPUTEFORMALADDRESS,
+      COMPUTEGLOBALADDRESS,
+      DEL,
+      DIV,
       JUMPI,
       LOADI,
       LOADAI,
       LOADGLOBAL,
       LOADINARGUMENT,
       LOADRET,
-      COMPUTEFORMALADDRESS,
+      MULT,
+      NEW,
+      OR,
+      PRINT,
+      PRINTLN,
+      READ,
+      RET,
       RESTOREFORMAL,
-      COMPUTEGLOBALADDRESS,
+      RSUBI,
       STOREAI,
       STOREGLOBAL,
       STOREINAGRUMENT,
       STOREOUTARGUMENT,
       STORERET,
-      CALL,
-      RET,
-      NEW,
-      DEL,
-      PRINT,
-      PRINTLN,
-      READ
+      SUB,
+      XORI
    }
 }
