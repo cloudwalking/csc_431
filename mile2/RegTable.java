e009fdcffa1fd5eee4a03110ad4db6862abd7826
Drin <DrinMontana@gmail.com>
2010-04-26 03:51:20 -0700

    virtual register set

--- mile2/RegTable.java
+++ mile2/RegTable.java
@@ -4,11 +4,13 @@ public class RegTable {
    private Hashtable<String, Integer> table;
    private int next;
    private int returnRegister;
+   private int ccRegister;
    
    public RegTable() {
       table = new Hashtable<String, Integer>();
       returnRegister = 0;
-      next = 1;
+      ccRegister = 1;
+      next = 2;
    }
    
    public int newRegister() {
@@ -21,10 +23,23 @@ public class RegTable {
    }
    
    public int lookupId(String id) {
+      if (id == null) {
+         System.err.println(id + " not allocated in registers");
+         return -1;
+      }
+      Integer reg = table.get(id);
+      if (reg == null) {
+         System.err.println("invalid register allocated to " + id);
+         return -1;
+      }
       return ((Integer)table.get(id)).intValue();
    }
    
    public int getReturnRegister() {
       return returnRegister;
+   }
+
+   public int getCCRegister() {
+      return ccRegister;
    }
 }
