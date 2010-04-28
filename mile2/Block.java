9dab32576bafbc7c58582c07a431c3a3cfc204e1
Drin <DrinMontana@gmail.com>
2010-04-28 01:41:24 -0700

    fixed printing errors

--- mile2/Block.java
+++ mile2/Block.java
@@ -1,22 +1,25 @@
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class Block {
-   private ArrayList<Block> successors;
+   private LinkedList<Block> successors;
    private LinkedList<Instruction> instrs;
-   private ArrayList<String> labels;
+   private LinkedList<String> labels;
    private int entered, exited;
 
    public Block() {
-      successors = new ArrayList<Block>();
+      successors = new LinkedList<Block>();
       instrs = new LinkedList<Instruction>();
-      labels = new ArrayList<String>();
+      labels = new LinkedList<String>();
       entered = 0;
       exited = 0;
    }
 
+   public Block(String label) {
+      this();
+      labels.add(label);
+   }
+
    public void addILoc(LinkedList instructions) {
-System.out.println("Hello new instruction ;D " + instructions.toString());
       instrs.addAll(instructions);
    }
 
@@ -31,17 +34,9 @@ System.out.println("Hello new instruction ;D " + instructions.toString());
    public String printInstrs(LinkedList<Instruction> iLoc) {
       String myInstrs = "";
       for (Instruction i : iLoc) {
-         myInstrs += i.toString() + "\n";
+         myInstrs += "\t" + i.toString() + "\n";
       }
-      return myInstrs.trim();
-   }
-
-   public String printList(ArrayList<String> asmContents) {
-      String retStr = "";
-      for (String s: asmContents) {
-         retStr += asmContents + ", ";
-      }
-      return retStr.trim();
+      return myInstrs;
    }
 
    public void printBlock() {
@@ -73,7 +68,7 @@ System.out.println("Hello new instruction ;D " + instructions.toString());
    public String successors(Block head) {
       String children = "";
       for (Block b: head.successors) {
-         children += printList(b.labels);
+         children += b.labels.toString();
       }
       return children.trim();
    }
@@ -83,9 +78,9 @@ System.out.println("Hello new instruction ;D " + instructions.toString());
          return;
       Block curBlock = cfg.removeFirst();
       System.out.println("Block " + numBlock + ":");
-      System.out.println("   labels: " + printList(curBlock.labels));
+      System.out.println("   labels: " + curBlock.labels.toString());
       System.out.println("   successors are: " + successors(curBlock));
-      System.out.println("   contains iloc: " + printInstrs(instrs));
+      System.out.println("   contains iloc:\n" + printInstrs(curBlock.instrs));
       printBlockTree(++numBlock, cfg);
    }
 
