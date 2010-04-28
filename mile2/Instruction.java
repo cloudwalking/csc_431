42ab01e69228618b2af2caefa9772e7470c4d768
Drin <DrinMontana@gmail.com>
2010-04-28 01:43:06 -0700

    refactored, adding more operators to getOperator method, modified constructors, changed member variables to be a single opcode and a single linkedlist of InstrFields which is defined at the bottom of Instruction.java

--- mile2/Instruction.java
+++ mile2/Instruction.java
@@ -1,91 +1,161 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Intermediate code, 4-tuple
  * http://en.wikipedia.org/wiki/Three_address_code
  * Immediate values are stored as an integer in register2, rather
  * than as a register value.
+ * private class Label
+ * private class Register
+ * private class Immediate
  */
 public class Instruction {
    private Operator op;
-   private int register1;
-   private int register2;
-   private int result;
-   private int immediate;
-   private String label;
+   private LinkedList<InstrField> fields = new LinkedList<InstrField>();
    
-   private String type;
-   private ArrayList<String> fieldNames;
-   
-   public Instruction(Operator op) {
-      this(op, -1, -1, -1);
+/**
+ * This constructor will be used for return instructions.
+ */
+   public Instruction(String opString) {
+      this.op = getOperator(opString);
    }
    
-   public Instruction(Operator op, int result) {
-      this(op, -1, -1, result);
-   }
-
-   public Instruction(Operator op, String target) {
-      this(op, -1, -1, -1, target);
-   }
-   
-   /*public Instruction(Operator op, String type, ArrayList<String> fieldNames) {
-      this.op = op;
-      this.type = type;
-      this.fieldNames = fieldNames;
-   }*/
-   
-   public Instruction(Operator op, int result, int immediate) {
-      this(op, -1, immediate, result, null);
-   }
-   
-   public Instruction(Operator op, int register1, int register2, int result) {
-      this(op, register1, register2, result, null);
+/**
+ * This constructor will be used for call and JumpI instructions.
+ */
+   public Instruction(String opString, String target) {
+      this.op = getOperator(opString);
+      fields.add(new Label(target));
    }
 
-   public Instruction(Operator op, int register1, int register2, int result, String target) {
-      this.op = op;
-      this.register1 = register1;
-      this.register2 = register2;
-      this.result = result;
-      this.label = target;
+/**
+ * This constructor will be used for branch instructions.
+ */
+   public Instruction(String opString, int ccReg, String trueTarget, String falseTarget) {
+      this.op = getOperator(opString);
+      fields.add(new Register(ccReg));
+      fields.add(new Label(trueTarget));
+      fields.add(new Label(falseTarget));
+   }
+
+/**
+ * This constructor will be used for storeret, del, I/O instructions.
+ */
+   public Instruction(String opString, int destReg) {
+      this.op = getOperator(opString);
+      fields.add(new Register(destReg));
+   }
+
+/**
+ * This constructor will be used for loadi, and new instructions.
+ */
+   public Instruction(String opString, int imm, int destReg) {
+      this.op = getOperator(opString);
+      fields.add(new Immediate(imm));
+      fields.add(new Register(destReg));
    }
    
+/**
+ * This constructor will be used for add, div, mult, sub, and, or,and other
+ * instructions that have two source registers and a destination register
+ * Edit: This will also be used for instructions with immediates, immediates
+ * will be loaded into registers immediately before creating this instruction.
+ */
+   public Instruction(String opString, int srcReg1, int srcReg2, int destReg) {
+      this.op = getOperator(opString);
+      fields.add(new Register(srcReg1));
+      fields.add(new Register(srcReg2));
+      fields.add(new Register(destReg));
+   }
+   
+/**
+ * Returns OPcode representing the instruction, such as ADD, AND, GT, etc.
+ */
    public static Operator getOperator(String op) {
-      if (op.equals("AND"))
+      if (op.equals("PLUS"))
+         return Operator.ADD;
+      else if (op.equals("ADDI"))
+         return Operator.ADDI;
+      else if (op.equals("AND"))
          return Operator.AND;
-      else if (op.equals("OR"))
-         return Operator.OR;
+      else if (op.equals("CALL"))
+         return Operator.CALL;
       else if(op.equals("EQ"))
          return Operator.CBREQ;
-      else if (op.equals("LT"))
-         return Operator.CBRLT;
-      else if (op.equals("GT"))
-         return Operator.CBRGT;
-      else if (op.equals("NE"))
-         return Operator.CBRNE;
-      else if (op.equals("LE"))
-         return Operator.CBRLE;
       else if (op.equals("GE"))
          return Operator.CBRGE;
-      else if (op.equals("PLUS"))
-         return Operator.ADD;
-      else if (op.equals("MINUS"))
-         return Operator.SUB;
-      else if (op.equals("TIMES"))
-         return Operator.MULT;
+      else if (op.equals("GT"))
+         return Operator.CBRGT;
+      else if (op.equals("LE"))
+         return Operator.CBRLE;
+      else if (op.equals("LT"))
+         return Operator.CBRLT;
+      else if (op.equals("NE"))
+         return Operator.CBRNE;
+      else if (op.equals("COMP"))
+         return Operator.COMP;
+      else if (op.equals("COMPI"))
+         return Operator.COMPI;
+      else if (op.equals("CFA"))
+         return Operator.COMPUTEFORMALADDRESS;
+      else if (op.equals("CGA"))
+         return Operator.COMPUTEGLOBALADDRESS;
+      else if (op.equals("DEL"))
+         return Operator.DEL;
       else if (op.equals("DIVIDE"))
          return Operator.DIV;
-      return null;
+      else if (op.equals("JUMPI"))
+         return Operator.JUMPI;
+      else if (op.equals("LOADI"))
+         return Operator.LOADI;
+      else if (op.equals("LOADAI"))
+         return Operator.LOADAI;
+      else if (op.equals("LOADGLOBAL"))
+         return Operator.LOADGLOBAL;
+      else if (op.equals("LOADARG"))
+         return Operator.LOADINARGUMENT;
+      else if (op.equals("LOADRET"))
+         return Operator.LOADRET;
+      else if (op.equals("TIMES"))
+         return Operator.MULT;
+      else if (op.equals("NEW"))
+         return Operator.NEW;
+      else if (op.equals("OR"))
+         return Operator.OR;
+      else if (op.equals("PRINT"))
+         return Operator.PRINT;
+      else if (op.equals("PRINTLN"))
+         return Operator.PRINTLN;
+      else if (op.equals("READ"))
+         return Operator.READ;
+      else if (op.equals("RET"))
+         return Operator.RET;
+      else if (op.equals("RESTFORMAL"))
+         return Operator.RESTOREFORMAL;
+      else if (op.equals("SUBI"))
+         return Operator.RSUBI;
+      else if (op.equals("STAI"))
+         return Operator.STOREAI;
+      else if (op.equals("STGLBL"))
+         return Operator.STOREGLOBAL;
+      else if (op.equals("STIN"))
+         return Operator.STOREINARGUMENT;
+      else if (op.equals("STOUT"))
+         return Operator.STOREOUTARGUMENT;
+      else if (op.equals("STRET"))
+         return Operator.STORERET;
+      else if (op.equals("MINUS"))
+         return Operator.SUB;
+      else if (op.equals("XORI"))
+         return Operator.XORI;
+      else {
+         System.err.println("Invalid Instruction OPCode");
+         return null;
+      }
    }
 
    public String toString() {
-      String instrString = op + "";
-      instrString += register1 > -1 ? ", " + register1 : "";
-      instrString += register2 > -1 ? ", " + register2 : "";
-      instrString += result > -1 ? ", " + result : "";
-      instrString += label != null ? ", " + label : "";
-      return instrString;
+      return op + fields.toString();
    }
 
    public enum Operator {
@@ -122,10 +192,47 @@ public class Instruction {
       RSUBI,
       STOREAI,
       STOREGLOBAL,
-      STOREINAGRUMENT,
+      STOREINARGUMENT,
       STOREOUTARGUMENT,
       STORERET,
       SUB,
       XORI
    }
+
+private abstract class InstrField {
+   abstract Object getValue();
+}
+
+/**
+ * Represents labels/IDs that will eventually evaluate
+ * to be an offset in memory.
+ */
+private class Label extends InstrField {
+   private String name;
+   public Label(String l) { name = l; }
+   public String getValue() { return name; }
+   public String toString() { return name; }
+}
+
+/**
+ * Represents registers where values will be stored
+ * when not in memory.
+ */
+private class Register extends InstrField {
+   private Integer num;
+   public Register(Integer val) { num = val; }
+   public Integer getValue() { return num; }
+   public String toString() { return num.toString(); }
+}
+
+/**
+ * Represents raw number values in instructions.
+ */
+private class Immediate extends InstrField {
+   private Integer val;
+   public Immediate(Integer value) { val = value; }
+   public Integer getValue() { return val; }
+   public String toString() { return val.toString(); }
+}
+
 }
